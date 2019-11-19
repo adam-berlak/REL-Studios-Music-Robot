bcbdd24523b1ba4e4e6b58ac17ddf4ffd084b35e
adam-berlak <adamberlak@hotmail.com>
2019-11-18 23:38:56 -0700

    removed folder

--- MIDIReader/startReadMidi - Copy.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\adamb\github\REL-Studios-Music-Robot\MIDIReader\build\scripts-3.7\mididump.py "C:\Users\adamb\github\REL-Studios-Music-Robot\MIDIReader\mary.mid"
-pause
--- MIDIReader/startReadMidi.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\adamb\AppData\Local\Programs\Python\Python37-32\python.exe "C:\Users\adamb\github\REL-Studios-Music-Robot\MIDIReader\setup.py" install
-pause
